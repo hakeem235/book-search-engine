b7f19fa9de6d387422bba60ab9bcd06c51542f76
ahmed hakeem <hakeemkoo@hotmail.com>
2021-07-19 13:18:30 -0400

    fix issue

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-const { authMiddleware } = require('./utils/auth');
+
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
+
+// const routes = require('./routes');
 
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -26,14 +27,18 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  }
+  );
+
 });
